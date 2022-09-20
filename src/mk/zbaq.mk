3e096887390da27785adac95a70050d6e35c00dc
Sébastien Pierre <sebastien.pierre@gmail.com>
2022-09-20 18:52:06 +1200

    [Update] make: switching away from fd as options don't quite work

--- src/mk/zbaq.mk
+++ src/mk/zbaq.mk
@@ -1,24 +1,29 @@
 .SHELLFLAGS = -ec
 
-ZBAQ_PATH:=$(shell readlink -f $(dir $(lastword $(MAKEFILE_LIST))))
+ZBAQ_MAKEFILE=$(shell readlink -f $(lastword $(MAKEFILE_LIST)))
+ZBAQ_PATH?=$(shell readlink -f $(dir $(lastword $(MAKEFILE_LIST))))
 
 # --
 # `ZBAQ_BATCH_SIZE` defines the number of elements that come into a manifest batch.
-# This is used to workaround shell limit of argumnets, as we need to pass to zpaq the
+# This is used to workaround shell limit of arguments, as we need to pass to zpaq the
 # list of files as arguments.
 ZBAQ_BATCH_SIZE?=10000
 
 ZBAQ_INDEX_PATH:=$(ZBAQ_PATH)/index.zpaq
 ZBAQ_CONTENT_PATH:=$(ZBAQ_PATH)/content-???.zpaq
 
-EXCLUDES=
-ifneq ($(wildcard $(ZBAQ_PATH)/meta.mk),)
-include $(ZBAQ_PATH)/meta.mk
+GITIGNORE_PATH?=$(HOME)/.gitignore
+DEFAULT_IGNORED?=.deps/run
+GIT_IGNORED?=$(file <$(GITIGNORE_PATH))
+IGNORED+=$(foreach P,$(GIT_IGNORED) $(DEFAULT_IGNORED),$P)
+
+ifneq ($(wildcard $(ZBAQ_PATH)/config.mk),)
+include $(ZBAQ_PATH)/config.mk
 else
-$(error ERR Can't find $(ZBAQ_PATH)/meta.mk)
+$(error ERR Can't find $(ZBAQ_PATH)/config.mk)
 endif
 
-
+FIND_IGNORED:=$(foreach P,$(IGNORED),$(if $(findstring /,$P),-a -not -path '$P' -a -not -path '*/$P/*',-a -not -name '$P'))
 
 # FROM: <https://stackoverflow.com/questions/12340846/bash-shell-script-to-find-the-closest-parent-directory-of-several-files>
 cmd-common-path=printf "%s\n%s\n" $1 | sed -e 'N;s/^\(.*\).*\n\1.*$$/\1/'  | sed 's/\(.*\)\/.*/\1/'
@@ -29,22 +34,42 @@ BACKUP_ROOT:=$(shell $(call cmd-common-path,$(BACKUP_SOURCES)))
 ZPAQ=zpaq
 FD=fd
 
-manifest: $(ZBAQ_PATH)/manifest.lst
-	@echo "Manifest: $<"
-	echo "Batches: " $(ZBAQ_PATH)/manifest-*.lst
+# --
+# `make info` displays overall information about the
+info:
+	@
+	echo "Package:  $(ZBAQ_PATH)"
+	echo "Root:     $(BACKUP_ROOT)"
+	echo "Sources:  $(BACKUP_SOURCES)"
+	echo "Ignored:  $(foreach P,$(IGNORED),$P)"
+	if [ ! -z "$$(find "$(ZBAQ_PATH)" -maxdepth 1 -name 'manifest-*.lst' -printf 1 -quit)" ]; then
+		echo "Manifest: $(ZBAQ_PATH)/manifest.lst"
+		echo "Batches: " $(ZBAQ_PATH)/manifest-*.lst
+		echo "Files:    $$(wc -l $(ZBAQ_PATH)/manifest-*.lst | tail -n1 | awk '{print $$1}')"
+	else
+		echo "Manifest: ??? → run 'make -f $$(realpath --relative-to=$$(pwd) $(ZBAQ_MAKEFILE)) manifest' to produce it"
+	fi
 
-$(ZBAQ_PATH)/manifest.lst: .FORCE
-	@echo $(BACKUP_SOURCES)
-	# We create catalogue of all the files we need to manage using `fd`
-	TEMP="$$(mktemp)"
-	for SRC in $(BACKUP_SOURCES); do
-		env -C "$(BACKUP_ROOT)" fd --full-path $$SRC >> "$$TEMP"
-	done
+
+manifest: $(ZBAQ_PATH)/manifest.lst
+	@echo find "$(ZBAQ_PATH)" -maxdepth 1 -name 'manifest-*.lst' -exec cat {} ';'
+
+clean-manifest: .FORCE
+	@
 	# We clean any previously created manifest file
 	# FROM: <https://stackoverflow.com/questions/6363441/check-if-a-file-exists-with-a-wildcard-in-a-shell-script>
 	if [ ! -z "$$(find "$(ZBAQ_PATH)" -maxdepth 1 -name 'manifest-*.lst' -printf 1 -quit)" ]; then
 		rm $(ZBAQ_PATH)/manifest-*.lst
 	fi
+
+$(ZBAQ_PATH)/manifest.lst: clean-manifest .FORCE
+	echo $(BACKUP_SOURCES)
+	# We create catalogue of all the files we need to manage using `fd`
+	TEMP="$$(mktemp)"
+	for SRC in $(BACKUP_SOURCES); do
+		echo env -C "$(BACKUP_ROOT)" find "$$SRC" -type f -or -type d $(FIND_IGNORED) >> "$$TEMP"
+		env -C "$(BACKUP_ROOT)" find "$$SRC" -type f -or -type d $(FIND_IGNORED) >> "$$TEMP"
+	done
 	truncate --size 0 "$@"
 	# And now we split the TEMP file into chunks of ZBAQ_BATCH_SIZE lines.
 	env -C $(ZBAQ_PATH) split -l$(ZBAQ_BATCH_SIZE) -d --additional-suffix .lst --verbose "$$TEMP"  manifest- | grep "'" | cut -d "'" -f2 >> $@
@@ -63,7 +88,11 @@ all:
 ZBAQ_CONTENT_PATH:=$(ZBAQ_PATH)/content-???.zpaq
 backup:
 
-.PHONY: all backup
+.PHONY: all backup clean-manifest
+
+print-%: .FORCE
+	$(info $* =$(value $*))
+	$(info $*:=$($*))
 
 .ONESHELL:
 	@$()
